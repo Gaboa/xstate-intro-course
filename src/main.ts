45572d4c70929ce1686d7a7a7a103607d0f1fc76
Your Name <you@example.com>
2023-01-11 17:09:38 +0200

    Add start logic for bulb function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
+function lightBulb() {
+  let isLit = false
+  let isBroken = false
+
+  return {
+    state() {
+      return { isLit, isBroken }
+    },
+    toggle() {
+      isLit = !isLit
+    },
+    break() {
+      isBroken = true
+    }
+  }
+}
+
+const log = function () {
+  console.log("State: ", bulb.state())
+}
+
+const bulb = lightBulb()
+bulb.toggle()
+bulb.break()
+log()
+
 export {}
